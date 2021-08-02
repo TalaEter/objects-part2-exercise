d1c29c0639289b92a40612b4503490649ecafd91
TalaEter <talaeter@gmail.com>
2021-08-02 16:33:53 +0300

    changing the code to work as a prototype

--- index.js
+++ index.js
@@ -2,27 +2,34 @@ function StopWatch(){
 
     let startTime, endTime, running, duration =0;
 
-    this.start = function(){
-        if(running) throw new Error("Stopwatch is already running/started.");
-        startTime = new Date();
-        running =true;
-    };
-
-    this.stop = function(){
-        if(!running) throw new Error("Stopwatch is already not-running/stopped. ");
-        running = false;
-        endTime = new Date();
-        const seconds = (endTime.getTime() - startTime.getTime()) /1000 ;
-        duration+=seconds;
-    };
-
-    this.reset = function(){
-        running = false;
-        endTime = startTime = null;
-        duration = 0;
-    };
-
-    Object.defineProperty(this, 'duration', {get: function(){return duration;}});
+    Object.defineProperty(this, 'running', {get: function(){return running;}})
+    Object.defineProperty(this, 'startTime', {get: function(){return startTime;}})
+    Object.defineProperty(this, 'endTime', {get: function(){return endTime;}})
+    Object.defineProperty(this, 'duration', {
+        get: function(){return duration;},
+        set: function(value){duration = value;},
+        //writable:false
+    });
 
 
 }
+
+StopWatch.prototype.start = function(){
+    if(this.running) throw new Error("Stopwatch is already running/started.");
+    this.startTime = new Date();
+    this.running =true;
+};
+
+StopWatch.prototype.stop = function(){
+    if(!this.running) throw new Error("Stopwatch is already not-running/stopped. ");
+    this.running = false;
+    this.endTime = new Date();
+    const seconds = (this.endTime.getTime() - this.startTime.getTime()) /1000 ;
+    this.duration+=seconds;
+};
+
+StopWatch.prototype.reset = function(){
+    this.running = false;
+    this.endTime = this.startTime = null;
+    this.duration = 0;
+};
